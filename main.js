9b99ab7aee01e862e71b0c6475d092a66515c18b
kylexrose@gmail.com <kylexrose@gmail.com>
2021-04-05 14:07:48 -0400

    completed step 1

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
 // Setup 'tick' sound
 const tick = new Audio('sounds/tick.mp3');
+const hihat = new Audio('sounds/hi-hat.mp3');
+const kickdrum = new Audio('sounds/kick-drum.mp3');
+const snare = new Audio('sounds/snare-drum.mp3');
+const tock = new Audio('sounds/tock.mp3');
+let beatCount = 1
 
 // This function is called every 600ms
 function update() {
-
     // Play the 'tick' sound
-    tick.play();
+    console.log(beatCount);
+    if (beatCount !== 4){
+        tick.play();
+    }else{
+        tock.play();
+        beatCount = 0;
+    }
+
+    beatCount++;
 }
 
 // This function sets up update() to be called every 600ms
