74f0bc8ad8f522c263cb97af0ee3aeb8db5dbcdd
kylexrose@gmail.com <kylexrose@gmail.com>
2021-04-05 14:36:30 -0400

    added all sounds

--- main.js
+++ main.js
@@ -1,23 +1,42 @@
 // Setup 'tick' sound
 const tick = new Audio('sounds/tick.mp3');
 const hihat = new Audio('sounds/hi-hat.mp3');
-const kickdrum = new Audio('sounds/kick-drum.mp3');
+const kick = new Audio('sounds/kick-drum.mp3');
 const snare = new Audio('sounds/snare-drum.mp3');
 const tock = new Audio('sounds/tock.mp3');
+const metCheck = document.querySelector("#metCheck");
+const hihatCheck = document.querySelector("#hihatCheck");
+const snareCheck = document.querySelector("#snareCheck");
+const kickCheck = document.querySelector("#kickCheck");
+
 let beatCount = 1
+const beatsPerMeasure = 4;
 
 // This function is called every 600ms
 function update() {
     // Play the 'tick' sound
     console.log(beatCount);
-    if (beatCount !== 4){
-        tick.play();
-    }else{
-        tock.play();
-        beatCount = 0;
+    if(metCheck.checked){
+        if (beatCount !== beatsPerMeasure){
+            tick.play();
+        }else{
+            tock.play();
+        }
     }
-
-    beatCount++;
+    if(hihatCheck.checked){
+        hihat.play();
+    }
+    if(snareCheck.checked){
+        snare.play();
+    }
+    if(kickCheck.checked){
+        kick.play();
+    }
+    if (beatCount !== beatsPerMeasure){
+        beatCount++;
+    }else{
+        beatCount = 1;
+    }
 }
 
 // This function sets up update() to be called every 600ms
@@ -27,3 +46,4 @@ function setupUpdate() {
 
 // Call setupUpdate() once after 300ms
 setTimeout(setupUpdate, 300);
+ 
